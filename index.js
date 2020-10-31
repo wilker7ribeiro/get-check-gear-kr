c608155c00f5881a67d2ccdab248f1f38d44a714
Wilker Ribeiro de Oliveira dos Santos <wilker7ribeiro@gmail.com>
2020-10-31 15:06:27 -0300

    FIX

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const { JSDOM } = jsdom;
 const nameToImage = require('./img-src-name.json')
 const imageToName = Object.assign({}, ...Object.entries(nameToImage).map(([a,b]) => ({ [b]: a })))
 
-const GEAR_ID = process.argv[3];
+const GEAR_ID = process.argv[2];
 
 (async function () {
   try {
+    console.log('Realizando requisição...')
     const response = await got("http://getamped.windyzone.com/sub_main/menu/item/view/item_accessory_view.ws?q="+GEAR_ID);
+    console.log('Parseando dom...')
     const dom = new JSDOM(response.body);
     const objResult = {};
+    console.log('Verificando estilos...')
     Object.keys(imageToName).map(src => objResult[imageToName[src]] = false)
     Array.from(dom.window.document.querySelectorAll('ul.wear_cha > li > p > img'))
       .forEach(img=> objResult[imageToName[img.src]] = true)
+    console.log('Finalizado')
     console.log(objResult);
-    Object.keys(objResult).forEach(key => console.log(key, '=' , objResult[key]));
   } catch (err) {
     throw err;
   }
